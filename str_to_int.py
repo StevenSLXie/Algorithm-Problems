5899d36c00c8026e8445a6fde0c231cc85f9e25d
Steven S.L. Xie <stevenslxie@gmail.com>
2014-12-27 16:17:19 +0800

    Update str_to_int.py

--- str_to_int.py
+++ str_to_int.py
@@ -1,27 +1,40 @@
-# given a string, convert it to a integer
-# for example, from '124'to 124
+# code example for Section 1.3 字符串转换成整数
+
+# Given a string, convert it to a integer without using the int()
+# For example, from '124'to 124
+
+
+# Author: Steven S.L. Xie
+# Email: stevenslxie at gmail.com
+# Last updated: 2014, Dec, 27
+def empty_check(s):
+    if s == '' or s is None:
+        return False
+    return True
+        
+def sign_check(n):
+    if n[0] == '-':
+        n.pop(0)
+        return n, -1
+    return n,1
+    
+def illegal_symbol_check(char):
+    if not (char>=0 and char<=9):
+        return False
+    return True
 
 def str_to_int(s):
-    # empty check
-    if s == '':
-        return 'The input is illegal!'
-        
+    if not empty_check(s):
+        return 'Illegal input.'
     n = list(s)
-    
-    # sign check
-    flag = 1
-    if n[0] == '-':
-        flag = -1
-        n.pop(0)
-        
+    n, flag = sign_check(n)
     l = len(n)
     
     sum = 0
     for i in range(l):
-        
         tmp = ord(n[l-i-1])-ord('0')
         
-        if not (tmp>=0 and tmp<=9):
+        if not illegal_symbol_check(tmp):
             return 'Illegal symbols'
             
         sum += 10**i*tmp
@@ -33,3 +46,4 @@ print str_to_int('')
 print str_to_int('  ')
 print str_to_int('-125964')
 print str_to_int('1259648888888888888888888888')
+
